db9940c39f5b37fa28a8b091822f871498df0bb8
TheLastNox <rony.dormevil@gmail.com>
2020-11-24 12:16:19 +0100

    Update to another new project

---
+++ src/main/java/group14/gestionnaire_livres/GestionnaireLivresApplication.java
@@ -0,0 +1,13 @@
+package group14.gestionnaire_livres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireLivresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionnaireLivresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group14/gestionnaire_livres/GestionnaireLivresApplicationTests.java
@@ -0,0 +1,13 @@
+package group14.gestionnaire_livres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireLivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
